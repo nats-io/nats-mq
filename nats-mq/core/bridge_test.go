f753c240c00d1900143061a626ef1b9c169e0575
Stephen Asbury <sasbury@synadia.com>
2019-04-16 11:23:03 -0700

    Added copyright header
    Minor fix for unit test that failed if gnatsd was running locally.

--- nats-mq/core/bridge_test.go
+++ nats-mq/core/bridge_test.go
@@ -1,3 +1,15 @@
+// Copyright 2012-2019 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package core
 
 import (
@@ -17,6 +29,14 @@ func TestStartBridgeNoNats(t *testing.T) {
 	config := conf.DefaultBridgeConfig()
 	config.Monitoring = conf.MonitoringConfig{
 		HTTPPort: -1,
+	}
+
+	// Use a bogus server because we don't want to default to one on the test machine
+	config.NATS = conf.NATSConfig{
+		Servers:        []string{"nats://abc:321"},
+		ConnectTimeout: 2000,
+		ReconnectWait:  2000,
+		MaxReconnects:  5,
 	}
 
 	bridge := NewBridgeServer()
